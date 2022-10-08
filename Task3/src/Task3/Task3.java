05b06e4c113d91586a44f0a2b800bfe7d02d9210
ArtemKonchyts <artem.kon2003@mail.ru>
2022-10-08 14:34:09 +0300

    Complete Task3

---
+++ Task3/src/Task3/Task3.java
@@ -0,0 +1,21 @@
+package Task3;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        calculateAndPrint(3, 5, 1);
+        calculateAndPrint(7, 145, 5);
+    }
+
+    static void calculateAndPrint(int a, int b, double h) {
+        double current = a;
+        System.out.printf("%5cx%4c|%4cF(x)%3c\n", ' ', ' ', ' ', ' ');
+        double result;
+        do {
+            result = Math.tan(current);
+            System.out.printf("%10f|%10f\n", current, result);
+            current += h;
+        } while (current <= b);
+    }
+}
